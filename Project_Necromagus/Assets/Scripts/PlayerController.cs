74a94c186feb3b2f5708e3c81794801ab1f7e3fc
Myrandagau <73671431+Myrandagau@users.noreply.github.com>
2021-10-08 03:48:41 -0400

    anims + prefabs + slicing tiles

--- Project_Necromagus/Assets/Scripts/PlayerController.cs
+++ Project_Necromagus/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerControls _playerControls;
 
-    [Header("Physics Related")] 
+    [Header("Physics Related")]
     public float JumpAndFallVelocity;
     public float Gravity = -9.8f;
     public float JumpForce = 15f;
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     public float ExtraDistanceValue = 0.34f;
     private float _movingSpeed = 4;
     private Vector3 _moveTowardsPos;
-    
+
     [Header("Sprite Leaning")]
     public Vector3 Center = Vector3.zero;
     public Vector3 Lean = new Vector3(0f, 0f, 5f);
@@ -35,19 +35,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private Transform _childTransform;
     private SpriteRenderer _childSpriteRenderer;
-    
+    private Animator _childAnimator;
+
 
     private bool _coroutineRunning;
-    
+
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         Collider2D = GetComponent<Collider2D>();
         _childTransform = transform.GetChild(0).GetComponent<Transform>();
         _childSpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
-        
+        _childAnimator = transform.GetChild(0).GetComponent<Animator>();
+
     }
-    
+
     private void Start()
     {
         _playerControls = PlayerControls.Instance;
@@ -63,23 +65,27 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         CheckForInteractionObject();
+        HandleAnimator();
     }
 
     private void HandleMovement()
     {
         Vector2 moveDirection = new Vector2(0, 0);
         if (_playerControls.GetMovingUp())
+        {
             HandleJump();
+        }
         moveDirection.x = -(_playerControls.GetMovingLeft()) + (_playerControls.GetMovingRight());
-        if(moveDirection.x == -1)
+        if (moveDirection.x == -1)
             HandleLean(true, Lean2);
-        if(moveDirection.x == 1)
+        if (moveDirection.x == 1)
             HandleLean(false, Lean);
         moveDirection.Normalize();
         if (moveDirection == Vector2.zero)
         {
             _childTransform.eulerAngles = Center;
         }
+
         _moveTowardsPos = new Vector2(moveDirection.x * _movingSpeed * JumpingSpeed, JumpAndFallVelocity);
         _rigidbody2D.velocity = _moveTowardsPos;
     }
@@ -98,16 +104,16 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(1f);
         _coroutineRunning = false;
     }
-    
+
     private void DecreaseJumpSpeed()
     {
         if (_coroutineRunning)
             if (JumpingSpeed > 1f)
-                JumpingSpeed -= Time.fixedDeltaTime*0.5f;
+                JumpingSpeed -= Time.fixedDeltaTime * 0.5f;
             else
                 JumpingSpeed = 1f;
     }
-    
+
     private void ResetJumpVariablesAndCr()
     {
         JumpAndFallVelocity = 0f;
@@ -124,7 +130,9 @@ public class PlayerController : MonoBehaviour
         if (CheckCeilingCollision())
             ResetJumpVariablesAndCr();
         if (!CheckIfGrounded())
-            JumpAndFallVelocity += (Gravity * _movingSpeed/2) * Time.deltaTime;
+        {
+            JumpAndFallVelocity += (Gravity * _movingSpeed / 2) * Time.deltaTime;
+        }
         else if (CheckIfGrounded())
         {
             if (JumpAndFallVelocity <= 0f)
@@ -142,6 +150,7 @@ public class PlayerController : MonoBehaviour
             {
                 StartCoroutine(DecreaseJumpSpeedIEnumerator());
                 JumpAndFallVelocity += (_movingSpeed * JumpForce) * 0.2f;
+
             }
         }
     }
@@ -151,14 +160,14 @@ public class PlayerController : MonoBehaviour
         Vector2 ray = _playerControls.GetMousePos();
         RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.zero, Mathf.Infinity, InteractionLayerMask);
         var successfulHit = false;
-        if (hit) 
+        if (hit)
         {
             Interactable interactable = hit.collider.GetComponent<Interactable>();
             if (interactable != null)
             {
                 HandleInteraction(interactable);
                 InteractionText.text = interactable.GetDescription();
-                var offset = new Vector2(0f,1f);
+                var offset = new Vector2(0f, 1f);
                 if ((Vector2)InteractionText.transform.position != (ray + offset))
                 {
                     InteractionText.transform.position = (ray + offset);
@@ -179,7 +188,7 @@ public class PlayerController : MonoBehaviour
                 {
                     //IF TRUE WE SHOW THEM THAT THEY CAN INTERACT WITH IT 
                     //THEN WE LOOK IF THEY INTERACT WITH IT
-                        //IF SO THEY CALL INTERACT
+                    //IF SO THEY CALL INTERACT
                 }
                 break;
             case Interactable.InteractionType.CLICK:
@@ -194,9 +203,9 @@ public class PlayerController : MonoBehaviour
     private RaycastHit2D CollisionCheck(float angle, Vector2 direction, float extraDistance)
     {
         RaycastHit2D hit = Physics2D.BoxCast(
-            Collider2D.bounds.center, 
-            Collider2D.bounds.size / 1.5f, 
-            angle, 
+            Collider2D.bounds.center,
+            Collider2D.bounds.size / 1.5f,
+            angle,
             direction,
             extraDistance,
             GroundLayerMask
@@ -205,12 +214,43 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool CheckIfGrounded()
-    { 
+    {
         return CollisionCheck(0f, Vector2.down, ExtraDistanceValue).collider != null;
     }
 
     private bool CheckCeilingCollision()
     {
         return CollisionCheck(0f, Vector2.up, ExtraDistanceValue).collider != null;
+    }
+
+    private void HandleAnimator() {
+        if (_playerControls.lockPlayer)
+        {
+            _childAnimator.SetBool("IsJumping", false);
+            _childAnimator.SetBool("IsIdle", false);
+            _childAnimator.SetBool("IsRunning", false);
+            _childAnimator.SetBool("IsDead", true);
+        }
+        else if (!CheckIfGrounded())
+        {
+            _childAnimator.SetBool("IsJumping", true);
+            _childAnimator.SetBool("IsIdle", false);
+            _childAnimator.SetBool("IsRunning", false);
+            _childAnimator.SetBool("IsDead", false);
+        }
+        else if (_rigidbody2D.velocity.x == 0)
+        {
+            _childAnimator.SetBool("IsJumping", false);
+            _childAnimator.SetBool("IsIdle", true);
+            _childAnimator.SetBool("IsRunning", false);
+            _childAnimator.SetBool("IsDead", false);
+        }
+        else
+        {
+            _childAnimator.SetBool("IsJumping", false);
+            _childAnimator.SetBool("IsIdle", false);
+            _childAnimator.SetBool("IsRunning", true);
+            _childAnimator.SetBool("IsDead", false);
+        }
     }
 }
