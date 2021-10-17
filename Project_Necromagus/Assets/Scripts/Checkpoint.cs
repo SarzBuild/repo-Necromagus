7c66f2041dca8dbd6acd46d1be4b89dc3ded759d
SarzBuild <jrmietn@gmail.com>
2021-10-16 22:01:33 -0400

    Update scene and checkpoint script

--- Project_Necromagus/Assets/Scripts/Checkpoint.cs
+++ Project_Necromagus/Assets/Scripts/Checkpoint.cs
@@ -8,25 +8,27 @@ public class Checkpoint : MonoBehaviour
     private CheckpointManager _checkpointManager;
     public LayerMask playerLayer;
     public Timer TimerReset;
+    private int _tmpTime;
 
     private void Start()
     {
         _checkpointManager = checkpointManagerObject.GetComponent<CheckpointManager>();
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if ((playerLayer.value & (1 << other.gameObject.layer)) > 0)
         {
             _checkpointManager.UpdateCheckpoint(transform.position);
+            _tmpTime = (int)TimerReset.timer;
         }
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if ((playerLayer.value & (1 << other.gameObject.layer)) > 0)
         {
-            TimerReset.timer = TimerReset.currentMaxTime;
+            TimerReset.timer = _tmpTime;
         }
     }
 }
